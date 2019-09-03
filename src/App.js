a4336881aa5bc541add65e6a66691def4acb4852
mulili <muli9611@gmail.com>
2019-09-03 22:24:20 +0800

    useState && useEffect

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  let [count, setCount] = useState(0);
+  useEffect(() => {
+    document.title = `${count} times`;
+    /**
+     * clear effect
+     *
+     * return ()=>{
+     *  clear method
+     * }
+     */
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App App-header">
+      <button onClick={() => setCount(count + 1)}>Click</button>
+      <p>Button has been clicked {count} times</p>
     </div>
   );
-}
+};
 
 export default App;
