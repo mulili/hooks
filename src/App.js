8d5379838c5361a187ff4edd75d374fbd1ec3cd5
mulili <muli9611@gmail.com>
2019-09-04 07:42:37 +0800

    init project with eslint+prettier+husky

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import './App.css';
 
 const App = () => {
-  let [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
   useEffect(() => {
     document.title = `${count} times`;
     /**
@@ -15,7 +15,9 @@ const App = () => {
   });
   return (
     <div className="App App-header">
-      <button onClick={() => setCount(count + 1)}>Click</button>
+      <button type="button" onClick={() => setCount(count + 1)}>
+        Click
+      </button>
       <p>Button has been clicked {count} times</p>
     </div>
   );
